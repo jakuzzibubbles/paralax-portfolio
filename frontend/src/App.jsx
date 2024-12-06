c3e5fde01497586999d1dadc2b7be3a53fff2c15
jakuzzibubbles <jakuzzi263@gmail.com>
2024-12-06 22:46:27 +0100

    styled cursor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -5,10 +6,28 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Skills from "./components/Skills";
-import ResumeSection from  "./components/ResumeSection";
+import ResumeSection from "./components/ResumeSection";
 import SceneInit from "./components/SceneInit";
 
 const App = () => {
+  useEffect(() => {
+    // Create the custom cursor element
+    const cursor = document.createElement("div");
+    cursor.classList.add("custom-cursor");
+    document.body.appendChild(cursor);
+
+    // Update the position of the custom cursor based on mouse movement
+    document.addEventListener("mousemove", (e) => {
+      cursor.style.left = `${e.pageX - cursor.offsetWidth / 2}px`;
+      cursor.style.top = `${e.pageY - cursor.offsetHeight / 2}px`;
+    });
+
+    // Clean up the cursor element when the component unmounts
+    return () => {
+      document.body.removeChild(cursor);
+    };
+  }, []);
+
   return (
     <Router>
       <div className="bg-slate-50">
