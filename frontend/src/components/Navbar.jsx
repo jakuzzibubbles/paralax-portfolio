a342210237adc977fecf25f27d97b17dce79ace7
jakuzzibubbles <jakuzzi263@gmail.com>
2024-12-08 14:46:12 +0100

    restyled navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,12 +10,16 @@ import Linux from "/assets/linux.jpg";
 import Myself from "/assets/myself.png";
 import awsQuest from "/assets/aws-quest.png";
 
-
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isItemsVisible, setIsItemsVisible] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleItemsVisibility = () => {
+    setIsItemsVisible(!isItemsVisible);
   };
 
   return (
@@ -25,8 +29,8 @@ const Navbar = () => {
       )}
 
       <div className="w-screen hidden md:flex items-center justify-center py-2 fixed top-0 z-10">
-        <nav className="w-full bg-pink-600/20 py-3 px-6 absolute left-0 top-0">
-          <ul className="flex items-bottom w-full">
+        <nav className="w-full bg-transparent py-3 px-6 absolute left-0 top-0">
+          <ul className="flex items-center w-full">
             <li>
               <Link
                 to="hero"
@@ -42,7 +46,7 @@ const Navbar = () => {
                 </div>
               </Link>
             </li>
-            <div className="ml-8 flex items-center justiry-center gap-3">
+            <div className="ml-8 flex items-center justify-center gap-3">
               <img
                 src={Myself}
                 alt="Profile"
@@ -53,32 +57,60 @@ const Navbar = () => {
                 alt="Linux"
                 className="w-[50px] h-[50px] md:w-[60px] md:h-[60px] transition-transform duration-300 ease-in-out hover:scale-150 mr-2"
               />
-
               <img
                 src={aws}
                 alt="AWS"
                 className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] transition-transform duration-300 ease-in-out hover:scale-150"
               />
-
               <img
                 src={awsQuest}
                 alt="AWS"
                 className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] transition-transform duration-300 ease-in-out hover:scale-150"
               />
-
               <img
                 src={awsKnowledge}
                 alt="AWS"
                 className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] transition-transform duration-300 ease-in-out hover:scale-150"
               />
-
               <img
                 src={awsBuilder}
                 alt="AWS"
                 className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] transition-transform duration-300 ease-in-out hover:scale-150"
               />
             </div>
-            <div className="flex flex-col gap-2 ml-auto px-4">
+
+          
+            <div className="realtive p-3 fixed top-6 right-2 z-10">
+              <button
+                onClick={toggleItemsVisibility}
+                className="w-11 h-11 text-white bg-pink-800/60 border border-pink-900/80 flex items-center justify-center rounded z-50"
+              >
+                {isItemsVisible ? (
+                  <RiCloseFill size={24} />
+                ) : (
+                  <CgMenuMotion size={24} />
+                )}
+              </button>
+            </div>
+
+         
+            <div className="p-3 fixed top-0 right-0 z-50 md:hidden">
+              <button
+                onClick={toggleMenu}
+                className="w-11 h-11 text-white bg-pink-800/60 border border-pink-900/80 flex items-center justify-center rounded z-50"
+              >
+                {isMenuOpen ? (
+                  <RiCloseFill size={24} />
+                ) : (
+                  <CgMenuMotion size={24} />
+                )}
+              </button>
+            </div>
+          </ul>
+
+          {isItemsVisible && (
+            <div className="flex flex-col gap-6 ml-auto px-4 bg-pink-800/60 w-1/3 fixed right-0 top-0 h-full items-center justify-center list-none">
+              <div className="ui-circle absolute top-0 left-0 -ml-20 -mt-10 mb-10 transition-opacity duration-300"></div>
               <li>
                 <Link
                   to="gallery"
@@ -86,7 +118,7 @@ const Navbar = () => {
                   duration={200}
                   spy
                   offset={-100}
-                  className="menu-item"
+                  className="font-semibold text-6xl text-white"
                 >
                   Gallery
                 </Link>
@@ -98,12 +130,11 @@ const Navbar = () => {
                   duration={200}
                   spy
                   offset={-100}
-                  className="menu-item"
+                  className="font-semibold text-6xl text-white"
                 >
                   Skills
                 </Link>
               </li>
-
               <li>
                 <Link
                   to="contact"
@@ -111,7 +142,7 @@ const Navbar = () => {
                   duration={200}
                   spy
                   offset={-100}
-                  className="menu-item"
+                  className="font-semibold text-6xl text-white"
                 >
                   Contact
                 </Link>
@@ -123,19 +154,21 @@ const Navbar = () => {
                   duration={200}
                   spy
                   offset={-100}
-                  className="menu-item"
+                  className="font-semibold text-6xl text-white"
                 >
                   Resume
                 </Link>
               </li>
             </div>
-          </ul>
+          )}
         </nav>
       </div>
-      <div className="p-3 fixed top-0 right-0 z-50">
+
+   
+      <div className="p-3 fixed top-0 right-0 z-50 md:hidden">
         <button
           onClick={toggleMenu}
-          className="w-11 h-11 text-white bg-pink-800/60 border border-pink-900/80 flex items-center justify-center rounded md:hidden z-50 ml-auto"
+          className="w-11 h-11 text-white bg-pink-800/60 border border-pink-900/80 flex items-center justify-center rounded z-50"
         >
           {isMenuOpen ? <RiCloseFill size={24} /> : <CgMenuMotion size={24} />}
         </button>
