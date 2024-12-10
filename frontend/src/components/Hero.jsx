348e0914422d1da12a3b42ebf87f8425ab410b04
Linh Nguyen <jakuzzi263@gmail.com>
2024-12-10 02:36:04 +0100

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
             done.
           </p>
 
-          <p>✅ ✅ ✅</p>
+          <p className="text-[100px]">🧁</p>
 
           <p className="mb-4">I look forward to discuss your next project.</p>
         </div>
